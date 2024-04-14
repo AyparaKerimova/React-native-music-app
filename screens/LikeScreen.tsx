7e1e45094d5f8d26c9b0388f42653f3d2b689d34
musayevnadir <msyv.nadir@gmail.com>
2024-04-14 21:09:20 +0400

    Created FlashList

--- screens/LikeScreen.tsx
+++ screens/LikeScreen.tsx
@@ -1,97 +1,103 @@
-import { View, Text,StyleSheet, ScrollView} from 'react-native'
-import React from 'react'
-import { Back } from '../icons/SVG'
-import { Profile } from '../components/Profile'
-import { colors } from '../assets/theme/colors'
-import { Cards } from '../components/Cards'
+/** @format */
 
+import { View, Text, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import React from "react";
+import { Back } from "../icons/SVG";
+import { Profile } from "../components/Profile";
+import { colors } from "../assets/theme/colors";
+import { Cards } from "../components/Cards";
+import { FlashList } from "@shopify/flash-list";
+import { songs } from "../mock/songs";
 
 export const LikeScreen = () => {
-    const songs =[{
-        id:1,
-        url:'https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1'
-    },
-    {
-        id:2,
-        url:'https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1'
-    },
-    {
-        id:3,
-        url:'https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1'
-    },
-    {
-        id:4,
-        url:'https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1'
-    },
-    {
-        id:5,
-        url:'https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1'
-    },
-    {
-        id:6,
-        url:'https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1'
-    },
-    {
-        id:7,
-        url:'https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1'
-    },
-
-]
   return (
-    <View style={styles.container}>
-      <Back/>
-      <Profile url='https://variety.com/wp-content/uploads/2015/12/gravity-falls.jpg?w=1000' title=' Aypara Kərimova' email='ayparakerimova004@gmail.com' role='Gold Member' description='Love Music and I am not an Musician.'/>
-      <View style={styles.albums}>
-      <Text style={styles.text}> Favourite Album</Text>
-      <ScrollView horizontal >
-       <View style={{flexDirection:'row',gap:9}}>
-        <Cards url='https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1' large={styles.large}/>
-        <Cards url='https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1' large={styles.large}/>
-        <Cards url='https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1' large={styles.large}/>
-        <Cards url='https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1' large={styles.large}/>
-        <Cards url='https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1' large={styles.large}/>
-        <Cards url='https://decider.com/wp-content/uploads/2016/10/gravity-falls-cult-corner.jpg?quality=75&strip=all&w=978&h=652&crop=1' large={styles.large}/>
+    <SafeAreaView style={styles.root}>
+      <ScrollView style={styles.container}>
+        <Back />
+        <View style={styles.profileContainer}>
+          <Profile
+            url="https://variety.com/wp-content/uploads/2015/12/gravity-falls.jpg?w=1000"
+            title=" Aypara Kərimova"
+            email="ayparakerimova004@gmail.com"
+            role="Gold Member"
+            description="Love Music and I am not an Musician."
+          />
+        </View>
+        <ScrollView style={{ paddingTop: 72 }} horizontal>
+          <FlashList
+            horizontal
+            scrollEnabled={false}
+            data={songs}
+            renderItem={({ item }) => (
+              <Cards title={item.title} url={item.url} />
+            )}
+            estimatedItemSize={200}
+            showsHorizontalScrollIndicator={false}
+            ItemSeparatorComponent={() => (
+              <View style={styles.flashGapHorizontal} />
+            )}
+          />
+        </ScrollView>
+        <View style={{ paddingTop: 74 }}>
+          <FlashList
+            scrollEnabled={false}
+            data={songs}
+            numColumns={3}
+            renderItem={({ item }) => <Cards url={item.url} />}
+            estimatedItemSize={200}
+            showsHorizontalScrollIndicator={false}
+            ItemSeparatorComponent={() => (
+              <View style={styles.flashGapVertical} />
+            )}
+          />
         </View>
       </ScrollView>
-
-      </View>
-      <View style={[styles.musics,{gap:9}]}>
-      <Text style={styles.text}> Favourite Music</Text>
-      <ScrollView  >
-       <View style={styles.musics}>
-        {
-            songs.map((item)=> <Cards url={item.url} large={styles.large}/>)
-        }
-        </View>
-      </ScrollView>
-      </View>
-      </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container:{
-        marginLeft:21,
-        marginTop:54
-    },
-    albums:{
-        marginTop:72,
-        gap:-66
-    },
-    text:{
-        color:colors.white,
-        fontSize:18,
-        fontFamily:'nunito-semibold'
-    },
-    large:{
-        width:106,
-        height:111,
-        borderRadius:10
-    },
-    musics:{
-        flex:1,
-        flexDirection:'row',
-        flexWrap:'wrap',
-        gap:9
-    }
-})
+  root: {
+    flex: 1,
+    // backgroundColor: "green",
+  },
+  container: {
+    // backgroundColor: "red",
+    paddingHorizontal: 20,
+    // marginLeft: 21,
+    // marginTop: 54,
+  },
+
+  profileContainer: {
+    paddingTop: 24,
+    // backgroundColor: "pink",
+  },
+
+  flashGapHorizontal: {
+    width: 9,
+  },
+
+  flashGapVertical: {
+    height: 9,
+  },
+  //   albums: {
+  //     marginTop: 72,
+  //     // gap: -66,
+  //   },
+  //   text: {
+  //     color: colors.white,
+  //     fontSize: 18,
+  //     fontFamily: "nunito-semibold",
+  //   },
+  //   large: {
+  //     width: 106,
+  //     height: 111,
+  //     borderRadius: 10,
+  //   },
+  //   musics: {
+  //     flex: 1,
+  //     flexDirection: "row",
+  //     flexWrap: "wrap",
+  //     gap: 9,
+  //   },
+});
